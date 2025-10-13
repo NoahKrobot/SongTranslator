41295aa5b8381e47a755004f07ec6424596f94d7
NoahKrobot <noah@certus-sustavi.hr>
2025-10-13 10:37:08 +0100

    Add the lyrics service, and let it display data on the screen

--- SongTranslator/Components/Data/TranslatedSongData.cs
+++ SongTranslator/Components/Data/TranslatedSongData.cs
@@ -3,12 +3,14 @@
     public class TranslatedSongData
     {
         public string SongName { get; set; }
+        public string ArtistName { get; set; }
         public long OriginalLyrics { get; set; }
         public long TranslatedLyrics { get; set; }
 
-        public TranslatedSongData(string name, long original, long translated)
+        public TranslatedSongData(string songName, string artistName, long original, long translated)
         {
-            SongName = name;
+            SongName = songName;
+            ArtistName = artistName;
             OriginalLyrics = original;
             TranslatedLyrics = translated;
         }
---
+++ SongTranslator/Services/FetchLyricsService.cs
@@ -0,0 +1,25 @@
+﻿using RestSharp;
+using System.Threading.Tasks;
+
+namespace SongTranslator.Services
+{
+    public class FetchLyricsService
+    {
+        static readonly string Lyrics_URL = "https://api.lyrics.ovh/v1/";
+
+        public async Task<string> LyricsTextAsync(string artist, string title)
+        {
+            var client = new RestClient(Lyrics_URL);
+            var request = new RestRequest($"{artist}/{title}");
+
+            var response = await client.ExecuteAsync(request);
+
+            if (response.Content == null)
+            {
+                return "Response is null";
+            }
+
+            return response.Content;
+        }
+    }
+}
