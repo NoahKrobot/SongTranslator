ea5363b4a166c3d461cab4e5746ac2d4d15dbbc6
NoahKrobot <noah@certus-sustavi.hr>
2025-10-13 19:03:05 +0100

    Add languages in the dropdown

---
+++ SongTranslator/Components/Data/LanguagesData.cs
@@ -0,0 +1,16 @@
+﻿namespace SongTranslator.Components.Data
+{
+    public class LanguagesData
+    {
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+
+        public LanguagesData() {}
+
+        public LanguagesData(string code, string name)
+        {
+            Code = code;
+            Name = name;
+        }
+    }
+}
---
+++ SongTranslator/Services/TranslationService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using SongTranslator.Components.Data;
+
+namespace SongTranslator.Services
+{
+    public class TranslationService
+    {
+
+        static readonly string languagesData = "Services/languages.json";
+        public async Task<List<LanguagesData>> GetLanguagesToList()
+        {
+
+            string languageJson = await File.ReadAllTextAsync(languagesData);
+
+            if (languageJson == null)
+            {
+                return new List<LanguagesData>();
+            }
+
+            var languageData = JsonConvert.DeserializeObject<List<LanguagesData>>(languageJson);
+
+            if (languageData == null)
+            {
+                return new List<LanguagesData>();
+
+            }
+            
+            return languageData;
+        }
+    }
+
+  
+}
