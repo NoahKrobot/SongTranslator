78380b3a9219dc4f2c91c329fbf902273edf278c
NoahKrobot <noah@certus-sustavi.hr>
2025-10-13 09:09:28 +0100

    Add project files.

---
+++ SongTranslator/Components/Data/TranslatedSongData.cs
@@ -0,0 +1,16 @@
+﻿namespace SongTranslator.Components.Data
+{
+    public class TranslatedSongData
+    {
+        public string SongName { get; set; }
+        public long OriginalLyrics { get; set; }
+        public long TranslatedLyrics { get; set; }
+
+        public TranslatedSongData(string name, long original, long translated)
+        {
+            SongName = name;
+            OriginalLyrics = original;
+            TranslatedLyrics = translated;
+        }
+    }
+}
