322acf3939fa32d56b024fefc2a9e4b69f22727f
NoahKrobot <noah@certus-sustavi.hr>
2025-10-21 12:14:15 +0100

    Change youtube data to media data for abstraction and both video and lyrics

---
+++ SongTranslator/Components/Data/MediaData.cs
@@ -0,0 +1,34 @@
+﻿namespace SongTranslator.Components.Data
+{
+
+    //abstraction used for Firebase
+    public abstract class MediaData
+    {
+        public string Title { get; set; }
+        public string Source { get; set; }
+        public DateTime RetrievedAt { get; set; } = DateTime.UtcNow;
+
+    }
+
+    //public class MediaData
+    //{
+    //    public string Thumbnail { get; set; }
+    //    public string Title { get; set; }
+    //    public string VideoId { get; set; }
+    //}
+
+    public class YouTubeVideo : MediaData
+    {
+        public string Thumbnail { get; set; }
+        public string VideoId { get; set; }
+        public string Url => $"https://www.youtube.com/watch?v={VideoId}";
+    }
+
+    public class SongLyrics : MediaData
+    {
+        public string Artist { get; set; }
+        public string SongName { get; set; }
+        public string Lyrics { get; set; }
+
+    }
+}
--- SongTranslator/Components/Data/YouTubeVideo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SongTranslator.Components.Data
-{
-    public class YouTubeVideo
-    {
-        public string Thumbnail { get; set; }
-        public string Title { get; set; }
-        public string VideoId { get; set; }
-    }
-}
